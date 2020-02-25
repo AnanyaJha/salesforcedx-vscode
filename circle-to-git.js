40bcaa57ab30cc4ffc9559d76f84cb52919624e7
Ananya <a.jha@salesforce.com>
2020-02-24 19:17:57 -0800

    ok this should look better

--- circle-to-git.js
+++ circle-to-git.js
@@ -9,7 +9,6 @@ const GITHUB_API_URI = 'https://api.github.com';
 const CIRCLECI_API_URI = 'https://circleci.com/api/v1.1';
 
 const repo = process.env['CIRCLE_PROJECT_REPONAME'];
-const sha = process.env[''];
 const username = process.env['CIRCLE_PROJECT_USERNAME'];
 const prNumber = path.basename(
   process.env['CIRCLE_PULL_REQUEST'] || process.env['CI_PULL_REQUEST'] || ''
@@ -22,12 +21,13 @@ const cciArtifacts = shell.exec(
 ).stdout;
 const buildArtifactsJSON = JSON.parse(cciArtifacts);
 const text = 'Here are the Circle CI artifacts for this build:<br>';
-console.log('these are artifacts' + buildArtifactsJSON + 'end of artifacts');
-buildArtifactsJSON.forEach(artifactURL => {
-  const url = artifactURL.url;
-  const name = path.basename(artifactURL.url);
-  const htmlLink = `${text}<a href='${url}' target='_blank' download='extensions'>${name}</a>`;
-  shell.exec(
-    `curl -H "Authorization: token $GH_AUTH_TOKEN" --silent POST --data '{"body": "${htmlLink}"}' ${GITHUB_API_URI}/repos/${username}/${repo}/issues/${prNumber}/comments`
-  );
+buildArtifactsJSON.forEach(artifact => {
+  const url = artifact.url;
+  const extName = path.basename(artifact.url);
+  const htmlLink = `${text}<a href='${url}' target='_blank' download='extensions'>${extName}</a><br>`;
+  text += htmlLink;
 });
+
+shell.exec(
+  `curl -H "Authorization: token $GH_AUTH_TOKEN" --silent POST --data '{"body": "${htmlLink}"}' ${GITHUB_API_URI}/repos/${username}/${repo}/issues/${prNumber}/comments`
+);
